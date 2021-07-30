344ff7420713392a708350d43e0df529ec6a2df9
kirin2211 <52277540+kirin2211@users.noreply.github.com>
2021-07-30 18:55:18 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,155 +1,41 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
- 
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var indexRouter = require('./Routs/index.js')
+var topicRouter = require('./Routs/topic.js');
+var helmet = require('helmet');
+
+app.use(helmet());
+app.use(express.static('public'));           //정적인 파일 서비스
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.get('*'/* * = 들어오는 모든 요청*/, function (request, response, next) {
+  //fs.readdir('./data', function(error, filelist)를 줄임.
+  fs.readdir('./data', function (error, filelist) {
+    request.list = filelist;
+    next();
+  });
+});
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', indexRouter)
+app.use('/topic', topicRouter);
+// /topic으로 시작하는 주소를 가진 웹페이지들에게 topicRouter라고 하는 미들웨어를 적용하겠다.
 
-app.get('/', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    ); 
-    response.send(html);
-  });
+
+
+app.use(function (req, res, next) {             //404 Not Found
+  res.status(404).send('404 Not Found');
 });
 
-/*
-app.get('/', function(req,res){
-  return res.send('Hello world!');
-});
-*/
-app.get('/page/:pageId', function(request, response) {            //page id를 만들어주는 부분
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(filelist);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.writeHead(200);
-      response.end(html);
-    });
-  });
-});
- 
-
-
-app.get('/create', function(request,response){
-  fs.readdir('./data', function(error, filelist){
-    var title = 'WEB - create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 });
 
-app.post('/create_process', function(request, response){         //생성된 것을 받는 역할을 하는 코드
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.redirect('/?id = ${title}');
-      })
-  });
-})
-
-app.get('/update/:pageId', function(request,response){
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-      );
-      response.send(html);
-    });
-  });
-});
-
-
-app.post('/update_process',function(request, response){      //수정된 것을 받는 역할을 하는 코드
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.redirect('/?id = ${title}');
-        })
-      });
-  });
-});
-
-app.post('/delete_process', function(request,response){
-  var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.redirect('/?id = ${title}');
-          })
-      });
-})
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
 
@@ -158,3 +44,5 @@ response.writeHead(302, {Location: `/?id=${title}`});
 response.send();
 */
 //response.redirect('/?id = ${title}');
+
+//다른사람이 만든 소프트웨어를 이용해서 나만의 프로그램을 만든다.
