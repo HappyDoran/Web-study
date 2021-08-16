76c5c8d8eab14c351a38bc875d3c493f1000a278
kirin2211 <52277540+kirin2211@users.noreply.github.com>
2021-08-16 21:03:18 +0900

    Update index.js

--- WEB-Node.js/Routs/index.js
+++ WEB-Node.js/Routs/index.js
@@ -1,25 +1,28 @@
 var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var qs = require('querystring');
+var bodyParser = require('body-parser');
+var sanitizeHtml = require('sanitize-html');
+var compression = require('compression')
+var template = require('../lib/template.js');
 var router = express.Router();
-var template = require('../lib/template');
 
-
-router.get('/', function(request, response) { 
+//route, routing
+//app.get('/', (req, res) => res.send('Hello World!'))
+router.get('/', function (request, response) {
     var title = 'Welcome';
     var description = 'Hello, Node.js';
     var list = template.list(request.list);
     var html = template.HTML(title, list,
-      `
+        `
       <h2>${title}</h2>${description}
-      <img src="/img/hello.jpg" style="width:100px; display:block; margin-top:10px;">
+      <img src="/img/welcome.jpg" style="width:300px; display:block; margin-top:10px;">
       `,
-      `<a href="/topic/create">create</a>`
-      //정적인 파일 서비스
+        `<a href="/topic/create">create</a>`
     );
     response.send(html);
-  });
-/*
-app.get('/', function(req,res){
-  return res.send('Hello world!');
 });
-*/
+
 module.exports = router;
