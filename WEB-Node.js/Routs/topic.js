40faac5d2d9a8b06fbdb51f860729c6485cf060d
kirin2211 <52277540+kirin2211@users.noreply.github.com>
2021-08-16 21:03:39 +0900

    Update topic.js

--- WEB-Node.js/Routs/topic.js
+++ WEB-Node.js/Routs/topic.js
@@ -5,11 +5,29 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 
+router.get('/login', function (request, response) {
+  var title = 'WEB - login';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/topic/login_process" method="post">
+      <p><input type="text" name="email" placeholder="email"></p>
+      <p>
+        <input type = "password" name = "password" placeholder = "password">
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>`
+    `<a href="/create">create</a>`
+    , '');
+  response.writeHead(200);
+  response.send(html);
+});
 
 router.get('/create', function (request, response) {
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
       <form action="/topic/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -20,39 +38,25 @@ router.get('/create', function (request, response) {
         </p>
       </form>
     `, '');
-    response.send(html);
+  response.send(html);
 });
 
-router.post('/create_process', function (request, response) {         //생성된 것을 받아서 처리하는 역할을 하는 코드
-    /*
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.redirect('/?id = ${title}');
-        })
-    });
-    */
-    var post = request.body;
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.redirect(`/topic/${title}`);
-    });
+router.post('/create_process', function (request, response) {
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+    response.redirect(`/topic/${title}`);
+  });
 });
 
 router.get('/update/:pageId', function (request, response) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        var title = request.params.pageId;
-        var list = template.list(request.list);
-        var html = template.HTML(title, list,
-            `
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
         <form action="/topic/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -64,87 +68,57 @@ router.get('/update/:pageId', function (request, response) {
           </p>
         </form>
         `,
-            `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-        );
-        response.send(html);
-    });
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    );
+    response.send(html);
+  });
 });
 
-router.post('/update_process', function (request, response) {      //수정된 것을 받아서 처리하는 역할을 하는 코드
-    /*
-    var body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-          response.redirect('/?id = ${title}');
-        })
-      });
-    });
-    */
-    var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-            response.redirect(`/topic/${title}`);
-        })
-    });
+router.post('/update_process', function (request, response) {
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      response.redirect(`/topic/${title}`);
+    })
+  });
 });
 
 router.post('/delete_process', function (request, response) {
-    /*
-    var body = '';
-    request.on('data', function (data) {
-      body = body + data;
-    });
-    request.on('end', function () {
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function (error) {
-        response.redirect('/?id = ${title}');
-      })
-    });
-    */
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function (error) {
-        response.redirect('/');
-    });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function (error) {
+    response.redirect('/');
+  });
 });
 
-router.get('/:pageId', function (request, response, next) {            //page id를 만들어주는 부분
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        if (err) {
-            next(err);
-        } else {
-            var title = request.params.pageId;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-                allowedTags: ['h1']
-            });
-            var list = template.list(request.list);
-            var html = template.HTML(sanitizedTitle, list,
-                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                ` <a href="/topic/create">create</a>
+
+router.get('/:pageId', function (request, response, next) {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    if (err) {
+      next(err);
+    } else {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ['h1']
+      });
+      var list = template.list(request.list);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/topic/create">create</a>
             <a href="/topic/update/${sanitizedTitle}">update</a>
             <form action="/topic/delete_process" method="post">
               <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
             </form>`
-            );
-            response.send(html);
-        }
-    });
+      );
+      response.send(html);
+    }
+  });
 });
 module.exports = router;
